f7a6229514906d3eafe447f6e1669b1ce2e5511d
github username <github email address>
2018-08-28 16:07:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
-
+require "pry"
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do  ##method name must be same the the one in form (line 8 in create_puppy)
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    @puppy = Puppy.new(@name, @breed, @age)
+    erb :display_puppy
+  end
+
 
 end
