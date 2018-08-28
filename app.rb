72654486e5834827d58944789c71c9bc78e7e839
Jesse McReady <jessemcready@gmail.com>
2018-08-28 11:29:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end # home route
+
+  post '/' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end # handle new puppy submission
+
+  get '/new' do
+    erb :create_puppy
+  end # new puppy route
+
 end
